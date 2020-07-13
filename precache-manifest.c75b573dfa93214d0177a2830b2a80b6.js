9f1506515743d9998677639d287ad398182a984c
Kyle Patterson <Kyle Patterson>
2020-07-13 17:39:42 -0500

    Updates

---
+++ precache-manifest.c75b573dfa93214d0177a2830b2a80b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9686cf99355f5a6d4361e10bf09595a6",
+    "url": "/fretboard-quiz/index.html"
+  },
+  {
+    "revision": "dd2a6f99c26a14a3ce24",
+    "url": "/fretboard-quiz/static/css/main.e8355ea8.chunk.css"
+  },
+  {
+    "revision": "0251858fe6c085c1933d",
+    "url": "/fretboard-quiz/static/js/2.5ff958fe.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/fretboard-quiz/static/js/2.5ff958fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2a6f99c26a14a3ce24",
+    "url": "/fretboard-quiz/static/js/main.1a55e40a.chunk.js"
+  },
+  {
+    "revision": "1187fd4182e806dc2ccf",
+    "url": "/fretboard-quiz/static/js/runtime-main.9ebc0550.js"
+  }
+]);
